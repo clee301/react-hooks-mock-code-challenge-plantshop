4e4ff6e78b8360eb0d738dcde4c98d7e15cc6819
Chan Lee <clee09818@gmail.com>
2022-10-30 17:08:45 -0400

    commit tree fba2e544f513292877516e287d9cbedc51493999

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+
+  const [plantItem, setPlantItem] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then((r) => r.json())
+      .then((data) => {
+        setPlantItem(data)
+      })
+  }, [])
+
+console.log(plantItem)
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantItem.map((item) => 
+      (<PlantCard key={item.id} item={item} />)
+    )}</ul>
   );
 }
 
